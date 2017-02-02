74d4288b31b2837bfd9d1606aa68c2971eda2c7e
guzmanasir <jesus.guzman787898@gmail.com>
2017-02-02 21:09:50 +0100

    estructura nueva

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./server/routes/index');
+var users = require('./server/routes/users/users');
+var admins = require('./server/routes/admins/admins');
+
 
 var app = express();
 
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'website'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -24,8 +27,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'private')));
 
 app.use('/', index);
+app.use('/admin', admins);
 app.use('/users', users);
 // catch 404 and forward to error handler
 /**
@@ -51,7 +56,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('views/layout');
 });
 
 module.exports = app;
