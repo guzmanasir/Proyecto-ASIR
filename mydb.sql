3150941b6daf30b227ad0130baef6d31a9947e80
guzmanasir <jesus.guzman787898@gmail.com>
2017-05-26 13:32:38 +0200

    estilos

---
+++ mydb.sql
@@ -0,0 +1,223 @@
+-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	5.7.18-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contiene`
+--
+
+DROP TABLE IF EXISTS `contiene`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contiene` (
+  `enlace_idenlace` int(11) NOT NULL,
+  `lista_idlista` int(11) NOT NULL,
+  `lista_usuario_id` int(11) NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`enlace_idenlace`,`lista_idlista`,`lista_usuario_id`),
+  KEY `fk_enlace_has_lista_lista1_idx` (`lista_idlista`,`lista_usuario_id`),
+  KEY `fk_enlace_has_lista_enlace1_idx` (`enlace_idenlace`),
+  CONSTRAINT `fk_enlace_has_lista_enlace1` FOREIGN KEY (`enlace_idenlace`) REFERENCES `enlace` (`idenlace`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_enlace_has_lista_lista1` FOREIGN KEY (`lista_idlista`, `lista_usuario_id`) REFERENCES `lista` (`idlista`, `usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contiene`
+--
+
+LOCK TABLES `contiene` WRITE;
+/*!40000 ALTER TABLE `contiene` DISABLE KEYS */;
+INSERT INTO `contiene` VALUES (44,46,2,0),(45,46,2,0),(46,46,2,0),(47,46,2,0),(48,46,2,0),(49,46,2,0),(50,46,2,0),(51,47,2,0),(52,47,2,0),(53,47,2,0),(54,47,2,0),(55,47,2,0),(56,47,2,0);
+/*!40000 ALTER TABLE `contiene` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `enlace`
+--
+
+DROP TABLE IF EXISTS `enlace`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `enlace` (
+  `idenlace` int(11) NOT NULL AUTO_INCREMENT,
+  `URL` varchar(45) NOT NULL,
+  `artista` varchar(45) NOT NULL,
+  `cancion` varchar(45) NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `thumbnail` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idenlace`),
+  UNIQUE KEY `URL_UNIQUE` (`URL`)
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enlace`
+--
+
+LOCK TABLES `enlace` WRITE;
+/*!40000 ALTER TABLE `enlace` DISABLE KEYS */;
+INSERT INTO `enlace` VALUES (44,'https://www.youtube.com/watch?v=Z-48u_uWMHY','Kendrick Lamar ',' Alright',0,'https://i.ytimg.com/vi/Z-48u_uWMHY/default.jpg'),(45,'https://www.youtube.com/watch?v=dwUug-ucBLc','Childish Gambino','The Worst Guys',0,'https://i.ytimg.com/vi/dwUug-ucBLc/default.jpg'),(46,'https://www.youtube.com/watch?v=O601PVTd0g8','Logic ',' Young Jesus (Explicit) ft. Big Lenbo',0,'https://i.ytimg.com/vi/O601PVTd0g8/default.jpg'),(47,'https://www.youtube.com/watch?v=BoEKWtgJQAU','JAY Z, Kanye West ',' Otis ft. Otis Redding',0,'https://i.ytimg.com/vi/BoEKWtgJQAU/default.jpg'),(48,'https://www.youtube.com/watch?v=8mtA9GvpzwU','Chance The Rapper ',' Favorite Song (feat. Childish Gambino)',0,'https://i.ytimg.com/vi/8mtA9GvpzwU/default.jpg'),(49,'https://www.youtube.com/watch?v=qDMSGuYnmBM','Ayokay ',' Kings of Summer (Feat. Quinn XCII)',0,'https://i.ytimg.com/vi/qDMSGuYnmBM/default.jpg'),(50,'https://www.youtube.com/watch?v=fzzMOMkjm8A','J. Cole ',' Crooked Smile ft. TLC',0,'https://i.ytimg.com/vi/fzzMOMkjm8A/default.jpg'),(51,'https://www.youtube.com/watch?v=2ynWJlO22mg','KEN BOOTHE ',' PUPPET ON A STRING',0,'https://i.ytimg.com/vi/2ynWJlO22mg/default.jpg'),(52,'https://www.youtube.com/watch?v=UNiVNaCkqaw','Alton Ellis & The Flames ',' Dance Crasher',0,'https://i.ytimg.com/vi/UNiVNaCkqaw/default.jpg'),(53,'https://www.youtube.com/watch?v=46iYn6-Kljs','Toots and The Maytals ',' Pressure Drop',0,'https://i.ytimg.com/vi/46iYn6-Kljs/default.jpg'),(54,'https://www.youtube.com/watch?v=E0MQQBlwSjo','Desmond Dekker ',' Israelites  (HQ)',0,'https://i.ytimg.com/vi/E0MQQBlwSjo/default.jpg'),(55,'https://www.youtube.com/watch?v=DUI_tSkBnxA','Ken Parker ',' when your gone',0,'https://i.ytimg.com/vi/DUI_tSkBnxA/default.jpg'),(56,'https://www.youtube.com/watch?v=yjg6flu3zuc','Toots and the Maytals ',' 54',0,'https://i.ytimg.com/vi/yjg6flu3zuc/default.jpg');
+/*!40000 ALTER TABLE `enlace` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `etiqueta`
+--
+
+DROP TABLE IF EXISTS `etiqueta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `etiqueta` (
+  `idetiqueta` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`idetiqueta`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etiqueta`
+--
+
+LOCK TABLES `etiqueta` WRITE;
+/*!40000 ALTER TABLE `etiqueta` DISABLE KEYS */;
+INSERT INTO `etiqueta` VALUES (3,'reggae'),(4,'ska'),(5,'rocksteady'),(6,'rythm and blues'),(7,'blues'),(8,'soul'),(9,'rock'),(10,'rockabilly'),(11,'metal'),(12,'heavy metal'),(13,'pop'),(14,'country'),(15,'drum and bass'),(16,'jazz'),(17,'blues'),(18,'punk'),(19,'salsa'),(20,'latin'),(21,'reggaeton'),(22,'rap'),(23,'house'),(24,'\'50'),(25,'\'60'),(26,'\'80'),(27,'swing');
+/*!40000 ALTER TABLE `etiqueta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favorito`
+--
+
+DROP TABLE IF EXISTS `favorito`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `favorito` (
+  `usuario_id` int(11) NOT NULL,
+  `lista_idlista` int(11) NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`usuario_id`,`lista_idlista`),
+  KEY `fk_usuario_has_lista_lista1_idx` (`lista_idlista`),
+  KEY `fk_usuario_has_lista_usuario_idx` (`usuario_id`),
+  CONSTRAINT `fk_usuario_has_lista_lista1` FOREIGN KEY (`lista_idlista`) REFERENCES `lista` (`idlista`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuario_has_lista_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favorito`
+--
+
+LOCK TABLES `favorito` WRITE;
+/*!40000 ALTER TABLE `favorito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favorito` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lista`
+--
+
+DROP TABLE IF EXISTS `lista`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lista` (
+  `idlista` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idlista`,`usuario_id`),
+  KEY `fk_lista_usuario1_idx` (`usuario_id`),
+  CONSTRAINT `fk_lista_usuario1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lista`
+--
+
+LOCK TABLES `lista` WRITE;
+/*!40000 ALTER TABLE `lista` DISABLE KEYS */;
+INSERT INTO `lista` VALUES (46,'Lista de hip-hop',2,0),(47,'Jamaican Oldies',2,0);
+/*!40000 ALTER TABLE `lista` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pertenece`
+--
+
+DROP TABLE IF EXISTS `pertenece`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pertenece` (
+  `lista_idlista` int(11) NOT NULL,
+  `lista_usuario_id` int(11) NOT NULL,
+  `etiqueta_idetiqueta` int(11) NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`lista_idlista`,`lista_usuario_id`,`etiqueta_idetiqueta`),
+  KEY `fk_lista_has_etiqueta_etiqueta1_idx` (`etiqueta_idetiqueta`),
+  KEY `fk_lista_has_etiqueta_lista1_idx` (`lista_idlista`,`lista_usuario_id`),
+  CONSTRAINT `fk_lista_has_etiqueta_etiqueta1` FOREIGN KEY (`etiqueta_idetiqueta`) REFERENCES `etiqueta` (`idetiqueta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lista_has_etiqueta_lista1` FOREIGN KEY (`lista_idlista`, `lista_usuario_id`) REFERENCES `lista` (`idlista`, `usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pertenece`
+--
+
+LOCK TABLES `pertenece` WRITE;
+/*!40000 ALTER TABLE `pertenece` DISABLE KEYS */;
+INSERT INTO `pertenece` VALUES (46,2,22,0),(47,2,3,0),(47,2,4,0),(47,2,5,0);
+/*!40000 ALTER TABLE `pertenece` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (2,'admin','admin','admin@admin.com',0);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-26 13:32:06
